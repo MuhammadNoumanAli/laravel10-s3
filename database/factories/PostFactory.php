fdfcb49c675660ced23db66544e16c33ac3f6e91
MuhammadNoumanAli <bzumultan0@gmail.com>
2023-09-25 14:49:57 +0500

    first commit

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class PostFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Post::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'user_id' => User::factory(),
+            'title' => $this->faker->unique()->sentence(),
+            'slug' => $this->faker->unique()->slug(),
+            'description' => $this->faker->paragraphs(4, true), // Generate 4 paragraphs of text
+        ];
+    }
+}
